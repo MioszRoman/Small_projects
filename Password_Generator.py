70dcaca916fea779726fe02c584ecd22cc1e50e0
MiloszRoman <mioszroman.kontakt@gmail.com>
2025-04-18 19:36:23 +0200

    [Password_Generator] Create function to generate password

--- Password_Generator.py
+++ Password_Generator.py
@@ -1,3 +1,9 @@
 import string
 import random
 
+def generate_password(pass_len: int) -> str:
+    return ''.join([
+        random.choice(string.printable)
+        for _ in range(pass_len)
+    ])
+
