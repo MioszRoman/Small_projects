442a2c39b82378c802c78f5251f1de65b8a14ce1
MiloszRoman <mioszroman.kontakt@gmail.com>
2025-04-18 19:47:13 +0200

    [Password_Generator] Create a menu for this app

--- Password_Generator.py
+++ Password_Generator.py
@@ -1,6 +1,7 @@
 import string
 import random
 
+
 def generate_password(pass_len: int) -> str:
     """This function will generate a password"""
     return ''.join([
@@ -14,10 +15,22 @@ def write_password(local_password, filepath= "password.txt"):
     with open(filepath, "w") as file:
         file.writelines(local_password)
 
+
 def get_password(filepath = "password.txt"):
     """This function will read a password from a file password.txt"""
     with open(filepath, "r") as file:
         pass_local = file.readlines()
     return pass_local
 
+
+def main(number):
+    return generate_password(number)
+
+if __name__ == '__main__':
+    what_for = input("For what do you need a password: ")
+    len_of_password = input("Give me a number of amount for your password: ")
+    passwords = get_password()
+    password = main(len_of_password)
+    full_info = f"{what_for}: {password}\n"
+    print(full_info)
 
