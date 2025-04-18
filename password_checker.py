61274b415ca138cabe855fb0d1da532d9223f7dc
MiloszRoman <mioszroman.kontakt@gmail.com>
2025-04-18 19:21:23 +0200

    Add function for checking if number is in the password

--- password_checker.py
+++ password_checker.py
@@ -1,7 +1,20 @@
 password = input("Give me a new password: ")
 
+result = {}
+digit = False
+upper = False
 
 if len(password) > 8:
+    result["length"] = True
     print("Your password is long enough")
 else:
+    result["length"] = False
     print("Your password is not long enough")
+
+for i in password:
+    if i.isdigit():
+        digit = True
+
+result["digit"] = digit
+
+print(result)
