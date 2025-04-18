d1e16a0495da41f29741cfc9665cbf4d5c0d9876
MiloszRoman <mioszroman.kontakt@gmail.com>
2025-04-18 19:30:21 +0200

    Last refactor of the code

--- password_checker.py
+++ password_checker.py
@@ -6,10 +6,8 @@ upper = False
 
 if len(password) > 8:
     result["length"] = True
-    print("Your password is long enough")
 else:
     result["length"] = False
-    print("Your password is not long enough")
 
 for i in password:
     if i.isdigit():
