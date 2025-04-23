0f0e8e00971e1ebc4c1a5f2c2d565a362304a4ea
MiloszRoman <mioszroman.kontakt@gmail.com>
2025-04-23 21:26:12 +0200

    [TXT_to_PDF] Add content to every page from .txt files

--- PDF from txt/Txt_to_PDF.py
+++ PDF from txt/Txt_to_PDF.py
@@ -6,10 +6,19 @@ filepaths = glob.glob('txt/*.txt')
 
 pdf = FPDF(orientation='P', unit='mm', format="A4")
 
+
 for filepath in filepaths:
     filename = Path(filepath).stem
+
+    with open(filepath, 'r') as file:
+        content = file.read()
+
     pdf.add_page()
     pdf.set_font(family='Times', style='B', size=24)
-    pdf.cell(w=0, h=20, txt=filename.title())
+    pdf.cell(w=0, h=20, txt=filename.title(), ln=1)
+
+    pdf.set_font(family='Times', size=12)
+    pdf.multi_cell(w=0, h=6, txt=content)
+
 
 pdf.output("Output.pdf")
