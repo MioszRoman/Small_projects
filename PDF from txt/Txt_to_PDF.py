8969edf49926ed66ebb7abe6e4091a813b1add0d
MiloszRoman <mioszroman.kontakt@gmail.com>
2025-04-23 21:30:21 +0200

    [TXT_to_PDF] Make a comments

--- PDF from txt/Txt_to_PDF.py
+++ PDF from txt/Txt_to_PDF.py
@@ -2,23 +2,28 @@ from fpdf import FPDF
 import glob
 from pathlib import Path
 
+#Take every filepaths to .txt files
 filepaths = glob.glob('txt/*.txt')
 
+#Create a pdf file
 pdf = FPDF(orientation='P', unit='mm', format="A4")
 
-
+#Go through every text file
 for filepath in filepaths:
     filename = Path(filepath).stem
 
+    #Get the content from every text file
     with open(filepath, 'r') as file:
         content = file.read()
-
+    #Create a pages for each text files
     pdf.add_page()
     pdf.set_font(family='Times', style='B', size=24)
+    #Create a titles for each page
     pdf.cell(w=0, h=20, txt=filename.title(), ln=1)
 
     pdf.set_font(family='Times', size=12)
+    #Give a content for each page
     pdf.multi_cell(w=0, h=6, txt=content)
 
-
+#Create an output file
 pdf.output("Output.pdf")
