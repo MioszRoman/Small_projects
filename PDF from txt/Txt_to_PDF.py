62df8ce2643ba9bd73eb38e8826dd981f9dd14e9
MiloszRoman <mioszroman.kontakt@gmail.com>
2025-04-23 21:22:06 +0200

    [TXT_to_PDF] Add pages with titles and create a PDF file

--- PDF from txt/Txt_to_PDF.py
+++ PDF from txt/Txt_to_PDF.py
@@ -3,3 +3,13 @@ import glob
 from pathlib import Path
 
 filepaths = glob.glob('txt/*.txt')
+
+pdf = FPDF(orientation='P', unit='mm', format="A4")
+
+for filepath in filepaths:
+    filename = Path(filepath).stem
+    pdf.add_page()
+    pdf.set_font(family='Times', style='B', size=24)
+    pdf.cell(w=0, h=20, txt=filename.title())
+
+pdf.output("Output.pdf")
