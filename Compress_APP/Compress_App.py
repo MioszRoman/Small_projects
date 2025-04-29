42eeaefa1b21746442c8de26dee05077db8bf185
MiloszRoman <mioszroman.kontakt@gmail.com>
2025-04-29 18:48:06 +0200

    [Compress_App] Create a buttons for choosing files, place to save the output and compress button. Add this to the window

--- Compress_APP/Compress_App.py
+++ Compress_APP/Compress_App.py
@@ -7,7 +7,14 @@ label2 = Sg.Text("Choose a place where you want to save it: ")
 input_text1 = Sg.Input()
 input_text2 = Sg.Input()
 
+choose_button1 = Sg.FilesBrowse('Choose', key='files')
+choose_button2 = Sg.FolderBrowse('Choose', key='folder')
 
-window = Sg.Window('Compressor', layout=[[label1, input_text1],[label2, input_text2]])
+compress_button = Sg.Button('Compress')
+
+
+window = Sg.Window('Compressor', layout=[[label1, input_text1, choose_button1],
+                                         [label2, input_text2, choose_button2],
+                                         [compress_button]])
 
 window.read()
