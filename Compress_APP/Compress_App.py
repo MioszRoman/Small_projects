b251d2836aa8e3fec3a119b50a0fbb24b6d8ac5b
MiloszRoman <mioszroman.kontakt@gmail.com>
2025-04-29 18:45:08 +0200

    [Compress_App] Create a labels and inputs in the app window

--- Compress_APP/Compress_App.py
+++ Compress_APP/Compress_App.py
@@ -1,1 +1,13 @@
 import FreeSimpleGUI as Sg
+
+
+label1 = Sg.Text("Choose a file/files to compress: ")
+label2 = Sg.Text("Choose a place where you want to save it: ")
+
+input_text1 = Sg.Input()
+input_text2 = Sg.Input()
+
+
+window = Sg.Window('Compressor', layout=[[label1, input_text1],[label2, input_text2]])
+
+window.read()
