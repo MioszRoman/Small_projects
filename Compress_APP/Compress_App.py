fcbbfc28d4740bf1d6160ffae074897fc649fe3b
MiloszRoman <mioszroman.kontakt@gmail.com>
2025-04-29 18:51:37 +0200

    [Compress_App] Create function to make archive (to compress a files to a zip file

--- Compress_APP/Compress_App.py
+++ Compress_APP/Compress_App.py
@@ -1,4 +1,14 @@
 import FreeSimpleGUI as Sg
+import zipfile, pathlib
+
+def make_archive(filepaths, dest_dir):
+    dest_path = pathlib.Path(dest_dir, 'compressed.zip')
+    with zipfile.ZipFile(dest_path, 'w') as archive:
+        for filepath in filepaths:
+            filepath = pathlib.Path(filepath)
+            archive.write(filepath, arcname=filepath.name)
+
+
 
 
 label1 = Sg.Text("Choose a file/files to compress: ")
