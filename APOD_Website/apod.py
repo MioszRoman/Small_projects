1d2629d9daaa73d75cc4ca7bc23f0af242b506d8
MiloszRoman <mioszroman.kontakt@gmail.com>
2025-05-03 20:31:30 +0200

    [APOD_Website] Create webpage layout using streamlit

--- APOD_Website/apod.py
+++ APOD_Website/apod.py
@@ -8,10 +8,15 @@ url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
 response = requests.get(url)
 data = response.json()
 
-#Make a variables for every things
+#Create a variables
 date_of_today = data['date']
 title_of_photo = data['title']
 img_url = data['url']
 description = data['explanation']
 
-
+#Make a webpage layout using streamlit
+st.title("My APOD website")
+st.header(title_of_photo)
+st.subheader(date_of_today)
+st.image(img_url)
+st.text(description)
