ed5524a5585acaf14b84ced0c8fbb17c6001b505
MiloszRoman <mioszroman.kontakt@gmail.com>
2025-05-07 18:54:37 +0200

    [Converter] Create a cases

--- Converter/converter.py
+++ Converter/converter.py
@@ -1,6 +1,10 @@
 import FreeSimpleGUI as Sg
 
 
+def convert_to_meters(feet, inches):
+    pass
+
+
 label1 = Sg.Text("Enter a feet: ")
 input1 = Sg.InputText(tooltip="Enter feet", key="feet")
 
@@ -13,3 +17,22 @@ output_label = Sg.Text(key="output")
 
 window = Sg.Window("Converter", layout=[[label1, input1], [label2, input2],
                                         [convert_button, output_label], [exit_button]])
+
+while True:
+    event, values = window.read()
+    match event:
+        case 'Convert':
+            try:
+                feet = float(values['feet'])
+                inches = float(values['inches'])
+                result = convert_to_meters(feet, inches)
+                window['output'].update(value=result, text_color='green')
+            except ValueError:
+                Sg.popup("Please insert a values for feet and inches")
+
+        case 'Exit':
+            break
+        case Sg.WIN_CLOSED:
+            break
+
+window.close()
