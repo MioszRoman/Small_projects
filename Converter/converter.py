390d712fba6a4a6d5b55f69d5ce7073c912eeea0
MiloszRoman <mioszroman.kontakt@gmail.com>
2025-05-07 18:58:32 +0200

    [Converter] Add comments and change theme to black color

--- Converter/converter.py
+++ Converter/converter.py
@@ -2,11 +2,15 @@ import FreeSimpleGUI as Sg
 
 
 def convert_to_meters(feet, inches):
+    """This function convert feet and inches into meters"""
     meters = feet * 0.3048 + inches * 0.0254
     message = f"{meters:3.2f} m"
     return message
 
+#Change a theme to a black color
+Sg.theme("Black")
 
+#Create variables
 label1 = Sg.Text("Enter a feet: ")
 input1 = Sg.InputText(tooltip="Enter feet", key="feet")
 
@@ -17,6 +21,7 @@ convert_button = Sg.Button("Convert")
 exit_button = Sg.Button("Exit")
 output_label = Sg.Text(key="output")
 
+#Make a layout of this app
 window = Sg.Window("Converter", layout=[[label1, input1], [label2, input2],
                                         [convert_button, output_label], [exit_button]])
 
